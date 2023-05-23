736e35b9cae4952310ccd42cb9440dc05752a8d0
MacAir <macair@MacAirs-MacBook-Air.local>
2023-05-23 09:13:03 +0530

    after signup validation

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import Register from './pages/Register/register'
+
+function App() {
+  return (
+    <>
+    <section className='bg-gradient-to-br from-blue-300 to-white'>
+    <Register/>
+    </section>
+     
+    </>
+  )
+}
+
+export default App
